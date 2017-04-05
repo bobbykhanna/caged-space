812ca582b0176a26bc39e1d126f7f64c98bb04a0
tejisusarla <tsusarla@uncc.edu>
2017-04-05 19:32:52 -0400

    Added update beacon functionality
    Added update beacon functionality

--- Apps/CagedAdmin/src/pages/beacon-details-page/beacon-details-page.ts
+++ Apps/CagedAdmin/src/pages/beacon-details-page/beacon-details-page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { BeaconModel } from '../../models/beacon';
+import { FormBuilder, Validators } from '@angular/forms';
+import { BeaconService } from '../../providers/beacon-service';
+import { UtilityService } from '../../providers/utility-service';
 
 
 @Component({
@@ -8,10 +12,80 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class BeaconDetailsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  public isEditing: boolean = false;
+  public beacon: BeaconModel;
+  editBeaconForm: any;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BeaconDetailsPage');
+  constructor(private _beaconService: BeaconService, private _util: UtilityService, private _nav: NavController, private _navParams: NavParams, private _fb: FormBuilder) {
+
+    this.beacon = new BeaconModel();
+
+    this.editBeaconForm = this._fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      location: ['', Validators.required],
+      globalid: ['', Validators.required]
+    });
+
+  }
+
+  ionViewDidEnter() {
+
+    this.beacon = this._navParams.get('model');
+
+    this.editBeaconForm.value.name = this.beacon.name;
+    this.editBeaconForm.value.instrument = this.beacon.description;
+    this.editBeaconForm.value.description = this.beacon.location;
+    this.editBeaconForm.value.globalid = this.beacon.globalid;
+
+  }
+
+  // Display Edit Musician Form.
+  public toggleEditBeacon() {
+
+    if (this.isEditing == true) {
+
+      this.isEditing = false;
+
+    } else {
+
+      this.isEditing = true;
+
+    }
+
+  }
+
+  // Update Beacon's Info.
+  public editBeacon(isValid: boolean) {
+
+    if (isValid) {
+
+      // Instantiate spinner. 
+      this._util.StartSpinner('Updating Musician\'s Info...');
+
+      let updatedBeacon = this.editBeaconForm.value;
+      updatedBeacon.id = this.beacon.id;
+
+      this._beaconService.editBeacon(updatedBeacon)
+        .subscribe(beacon => {
+
+          this._util.StopSpinner();
+
+          this.beacon = beacon;
+
+          this.isEditing = false;
+
+        }, error => {
+
+          this._util.StopSpinner();
+
+          this._util.ShowAlert('Internal Error', 'Could not edit Musician.');
+
+        });
+
+    }
+
   }
 
 }
+
