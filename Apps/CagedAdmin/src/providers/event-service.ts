fa6c909937829b0db792679fcf3b16f4f6de36da
saransh bhardwaj <saranshbhardwaj@gmail.com>
2017-04-09 22:18:59 -0400

    Added upload image functionality in Add Event

--- Apps/CagedAdmin/src/providers/event-service.ts
+++ Apps/CagedAdmin/src/providers/event-service.ts
@@ -3,7 +3,9 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ConfigService } from '../providers/config-service';
+import { FileService } from '../providers/file-service';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { EventModel } from '../models/event';
 import { AddEventModel } from '../models/addEvent';
 import { AngularFire } from 'angularfire2';
@@ -21,7 +23,7 @@ export class EventService {
     events: Array<EventModel>
   };
 
-  constructor(private _http: Http, private _config: ConfigService, private _af: AngularFire) {
+  constructor(private _http: Http, private _config: ConfigService, private _af: AngularFire, private _fileService: FileService) {
 
     this._eventsStore = { events: new Array<EventModel>() };
 
@@ -55,12 +57,64 @@ export class EventService {
 
   }
 
-  public addEvent(model: AddEventModel): Observable<EventModel> {
+  // Add new Event.
+  public addEvent(model: EventModel, hasUploadedNewImage: boolean, profileImage: string): Promise<EventModel> {
 
-    return this._http.post(this._config.addEventUrl, model)
-      .map(res => {
-        return this._MapEvent(res);
-      });
+    let promise = new Promise<EventModel>((resolve, reject) => {
+
+      this._getNewEventId()
+        .subscribe(eventId => {
+
+          let newEvent = model;
+          newEvent.id = eventId;
+
+          if (hasUploadedNewImage) {
+
+            this._uploadEventProfileImage(eventId, profileImage).then(imageUrl => {
+
+              newEvent.eventImageUrl = imageUrl;
+
+              this._http.post(this._config.addMusicianUrl, newEvent).subscribe(response => {
+
+                resolve(this._MapEvent(response));
+
+              }, error => {
+
+                reject(error);
+
+              });
+
+            }).catch(error => {
+
+              reject(error);
+
+            });
+
+          } else {
+
+            newEvent.eventImageUrl = '../../assets/thumbnail-totoro.png';
+
+            this._http.post(this._config.addMusicianUrl, newEvent).subscribe(response => {
+
+              resolve(this._MapEvent(response));
+
+            }, error => {
+
+              reject(error);
+
+            });
+
+          }
+
+        }, error => {
+
+          reject(error);
+
+        });
+
+    });
+
+    return promise;
 
   }
 
@@ -70,5 +124,36 @@ export class EventService {
       .map(res => {
         return this._MapEvent(res);
       });
+  }
+
+    private _getNewEventId(): Observable<string> {
+
+    return this._http.get(this._config.getNewMusicianIdUrl) //Need to change the url
+      .map(res => {
+        return res.json().data;
+      });
+
+  }
+
+    private _uploadEventProfileImage(eventId: string, file: string): Promise<any> {
+
+    let promise = new Promise<any>((res, rej) => {
+
+      let fileName = 'resource_image' + this._fileService.getFileExtensionFromDataString(file);
+
+      this._fileService.uploadFile('events', eventId, fileName, file).then(function (imageUrl) {
+
+        res(imageUrl);
+
+      }).catch(function (error) {
+
+        rej(error);
+
+      });
+
+    });
+
+    return promise;
+
   }
 }
