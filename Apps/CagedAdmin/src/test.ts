681660dbffb68f315ac7e3e02b3f30cdfb77df22
iyeremuk <iyeremuk@gmail.com>
2017-04-27 19:53:30 -0400

    test helper class

--- Apps/CagedAdmin/src/test.ts
+++ Apps/CagedAdmin/src/test.ts
@@ -7,11 +7,27 @@ import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
 
+import { ConfigService } from './providers/config-service';
+import { EmailService } from './providers/email-service';
+import { UtilityService } from './providers/utility-service';
+import { UserService } from './providers/user-service';
+import { MusicianService } from './providers/musician-service';
+import { StreamService } from './providers/stream-service'
+import { EventService } from './providers/event-service';
+import { BeaconService } from './providers/beacon-service';
+import { FileService } from './providers/file-service';
+
+import { AngularFire } from 'angularfire2';
+import { AngularFireModule } from 'angularfire2';
+
+import { MockBackend, MockConnection } from "@angular/http/testing";
+import { BaseRequestOptions, Http, XHRBackend, HttpModule, ResponseOptions, RequestMethod } from "@angular/http";
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { getTestBed, TestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import { App, Config, Form, IonicModule, Keyboard, DomController, MenuController, NavController, Platform } from 'ionic-angular';
-import { ConfigMock } from './mocks';
+import { App, Config, Form, IonicModule, DomController, MenuController, NavController, Platform, LoadingController } from 'ionic-angular';
+import { ConfigMock, PlatformMock } from './mocks';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare var __karma__: any;
@@ -36,3 +52,82 @@ context.keys().map(context);
 
 // Finally, start Karma to run the tests.
 __karma__.start();
+
+export class TestUtils {
+
+    public static beforeEachCompiler(components: Array<any>): Promise<{ fixture: any, instance: any }> {
+        return TestUtils.configureIonicTestingModule(components)
+            .compileComponents().then(() => {
+                let fixture: any = TestBed.createComponent(components[0]);
+                return {
+                    fixture: fixture,
+                    instance: fixture.debugElement.componentInstance,
+                };
+            });
+    }
+
+    public static configureIonicTestingModule(components: Array<any>): typeof TestBed {
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyAgvU-ZNdAMYJaw_kTK-uyWMIGHwCZtmMM",
+            authDomain: "cagedspace-9d75f.firebaseapp.com",
+            databaseURL: "https://cagedspace-9d75f.firebaseio.com",
+            storageBucket: "cagedspace-9d75f.appspot.com",
+            messagingSenderId: "464147072174"
+        };
+
+        return TestBed.configureTestingModule({
+            declarations: [
+                ...components,
+            ],
+            providers: [
+                App,
+                Form,
+                DomController,
+                MenuController,
+                LoadingController,
+                NavController,
+                ConfigService,
+                EmailService,
+                UtilityService,
+                UserService,
+                EventService,
+                MusicianService,
+                StreamService,
+                BeaconService,
+                FileService,
+                AngularFire,
+                MockBackend,
+                BaseRequestOptions,
+                { provide: Platform, useClass: PlatformMock },
+                { provide: Config, useClass: ConfigMock },
+                {
+                    provide: Http,
+                    deps: [MockBackend, BaseRequestOptions],
+                    useFactory:
+                    (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+                        return new Http(backend, defaultOptions);
+                    }
+                }
+            ],
+            imports: [
+                FormsModule,
+                IonicModule,
+                ReactiveFormsModule,
+                HttpModule,
+                AngularFireModule.initializeApp(firebaseConfig),
+            ],
+        });
+    }
+
+    // http://stackoverflow.com/questions/2705583/how-to-simulate-a-click-with-javascript
+    public static eventFire(el: any, etype: string): void {
+        if (el.fireEvent) {
+            el.fireEvent('on' + etype);
+        } else {
+            let evObj: any = document.createEvent('Events');
+            evObj.initEvent(etype, true, false);
+            el.dispatchEvent(evObj);
+        }
+    }
+}
